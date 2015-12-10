be287188af81db5802e68557c6d49843bdaef1da
Michele Polonioli <m.polonioli@campus.unimib.it>
2015-12-10 16:21:47 +0100

    added method calculate(String, int, int)

--- Calculator/src/calculator/Calculator.java
+++ Calculator/src/calculator/Calculator.java
@@ -9,4 +9,8 @@ public class Calculator {
 	private void initHashMap(){
 		OperationHashMap.operation.put("+", Plus.plus);
 	}
+	
+	public int calculate(String operator, int operand_1, int operand_2){
+		return OperationHashMap.operation.get(operator).calculate(operand_1, operand_2);
+	}
 }
