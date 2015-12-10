0709a1ef7d7c4b8ba9afe1bfa8863ce4b4604067
Michele Polonioli <m.polonioli@campus.unimib.it>
2015-12-10 16:17:05 +0100

    added method initHashMap

--- Calculator/src/calculator/Calculator.java
+++ Calculator/src/calculator/Calculator.java
@@ -2,4 +2,11 @@ package calculator;
 
 public class Calculator {
 
+	public Calculator(){
+		this.initHashMap();
+	}
+	
+	private void initHashMap(){
+		OperationHashMap.operation.put("+", Plus.plus);
+	}
 }
